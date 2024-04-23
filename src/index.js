c76a257d128d40df1b3acf7963c6df2c3d04cbe2
Adrian <adriankotyra@yahoo.com>
2024-04-23 11:14:51 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import App from './App';
+import StartRating from "./components/starRating/starRating"
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+    {/* <StartRating maxRating={10} size={30} className={'test'} defaultRating={0}/>
+    <StartRating maxRating={5} size={60} messages={['Terrible', 'Bad', 'Medium', 'Good', 'Perfect']}/> */}
+  </React.StrictMode>
+);
+
